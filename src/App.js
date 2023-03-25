e88ce9ec2da56eca5afa9c6aaa0c040b747a9cad
justabayet <anthony.bayet@hotmail.com>
2023-03-25 17:12:44 +0100

    remove data file and usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import { data } from './data'
 import MainView from './Components/MainView'
 import { AuthenticationProvider } from './Providers/AuthenticationProvider'
 import { DatabaseProvider } from './Providers/DatabaseProvider'
@@ -13,9 +12,7 @@ function App() {
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <AuthenticationProvider>
         <DatabaseProvider>
-          <div style={{ maxWidth: 600, margin: '0 auto', padding: 20 }}>
-            <MainView data={data} />
-          </div>
+          <MainView />
         </DatabaseProvider>
       </AuthenticationProvider>
     </LocalizationProvider>
