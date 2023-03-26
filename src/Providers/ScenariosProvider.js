748d799d686b41784cd19a40f4cab114982f9fd4
justabayet <anthony.bayet@hotmail.com>
2023-03-26 17:45:06 +0200

    Use firestore converter for scenario

--- src/Providers/ScenariosProvider.js
+++ src/Providers/ScenariosProvider.js
@@ -1,6 +1,7 @@
 
 import { collection, getDocs } from "firebase/firestore"
 import { createContext, useContext, useEffect, useState } from "react"
+import { getFormattedDate } from "../helpers"
 import { useUserDoc } from "./UserDocProvider"
 
 class Scenarios {
@@ -10,7 +11,57 @@ class Scenarios {
     }
 }
 
+class Scenario {
+    constructor({ copyId, startDate, endDate, startAmount = 0, id, name }) {
+        this.copyId = copyId
+        this.startDate = startDate
+        this.endDate = endDate
+        this.startAmount = startAmount
+        this.name = name
+        this.id = id
+    }
+}
+
 const ScenariosContext = createContext(new Scenarios([]))
+
+const converter = {
+    toFirestore(scenario) {
+        console.log(scenario)
+        return {
+            startDate: getFormattedDate(scenario.startDate),
+            endDate: getFormattedDate(scenario.endDate),
+            startAmount: scenario.startAmount,
+            copyId: scenario.copyId,
+            name: scenario.name
+        };
+    },
+    fromFirestore(snapshot, options) {
+        const scenarioDb = snapshot.data()
+        const copyId = scenarioDb.copyId
+        let startDate = new Date(scenarioDb.startDate)
+        let endDate = new Date(scenarioDb.endDate)
+        let startAmount = parseInt(scenarioDb.startAmount)
+        let name = scenarioDb.name
+        const id = snapshot.id
+
+        if (scenarioDb.startDate === undefined || startDate === "Invalid date") {
+            startDate = new Date()
+        }
+
+        if (scenarioDb.endDate === undefined || endDate === "Invalid date") {
+            endDate = new Date(startDate)
+            endDate.setFullYear(startDate.getFullYear() + 1)
+        }
+
+        if (isNaN(startAmount)) {
+            startAmount = 0
+        }
+
+        // TODO check copyId is valid
+
+        return new Scenario({ copyId, startDate, endDate, startAmount, id, name })
+    }
+}
 
 export const ScenariosProvider = (props) => {
     const { userDoc } = useUserDoc(null)
@@ -20,7 +71,7 @@ export const ScenariosProvider = (props) => {
 
     useEffect(() => {
         if (userDoc) {
-            setScenariosCollection(collection(userDoc, 'scenarios'))
+            setScenariosCollection(collection(userDoc, 'scenarios').withConverter(converter))
         } else {
             setScenariosCollection(null)
         }
@@ -35,10 +86,9 @@ export const ScenariosProvider = (props) => {
 
                     const scenariosQueried = []
                     querySnapshot.forEach(doc => {
-                        const data = doc.data()
-                        data.id = doc.id
-                        scenariosQueried.push(data)
+                        scenariosQueried.push(doc.data())
                     })
+                    console.log(scenariosQueried)
 
                     setScenarios(scenariosQueried)
                 })
