997f6d873afb727388ffd0368037beb4473a7c67
justabayet <anthony.bayet@hotmail.com>
2023-03-31 00:08:03 +0200

    Improve graph visual

--- src/Providers/GraphProvider.js
+++ src/Providers/GraphProvider.js
@@ -71,6 +71,17 @@ export const GraphProvider = (props) => {
         if (chartRef.current) {
             chartRef.current.destroy()
         }
+        const colorExpense = '132, 94, 194'
+        const colorValue = '214, 93, 177'
+        const colorTarget = '255, 111, 145'
+        /**
+            #845EC2 : rgba(132, 94, 194, 1)
+            #D65DB1 : rgba(214, 93, 177, 1)
+            #FF6F91 : rgba(255, 111, 145, 1)
+            #FF9671 : rgba(255, 150, 113, 1)
+            #FFC75F : rgba(255, 199, 95, 1)
+            #F9F871 : rgba(249, 248, 113, 1)
+         */
         chartRef.current = new Chart(ctx, {
             type: 'line',
             data: {
@@ -78,26 +89,26 @@ export const GraphProvider = (props) => {
                     {
                         label: 'Target',
                         data: [],
-                        backgroundColor: 'rgba(230, 0, 230, 0.5)',
-                        borderColor: 'rgba(230, 0, 230, 1)',
+                        backgroundColor: `rgba(${colorTarget}, 0.2)`,
+                        borderColor: `rgba(${colorTarget}, 1)`,
                         borderWidth: 2,
-                        pointRadius: 5,
+                        pointRadius: 0,
                         fill: false,
                     },
                     {
                         label: 'Value',
                         data: [],
-                        backgroundColor: 'rgba(230, 230, 0, 0.5)',
-                        borderColor: 'rgba(230, 230, 0, 1)',
+                        backgroundColor: `rgba(${colorValue}, 0.2)`,
+                        borderColor: `rgba(${colorValue}, 1)`,
                         borderWidth: 2,
-                        pointRadius: 5,
-                        fill: false,
+                        pointRadius: 0,
+                        fill: true,
                     },
                     {
                         label: 'Expense',
                         data: [],
-                        backgroundColor: 'rgba(100, 0, 230, 0.5)',
-                        borderColor: 'rgba(100, 0, 230, 1)',
+                        backgroundColor: `rgba(${colorExpense}, 0.2)`,
+                        borderColor: `rgba(${colorExpense}, 1)`,
                         borderWidth: 2,
                         pointRadius: 0,
                         fill: true,
@@ -122,6 +133,9 @@ export const GraphProvider = (props) => {
                                 month: 'MMM YYYY',
                             },
                         },
+                        ticks: {
+                            maxTicksLimit: 10 // Set the maximum number of ticks to 10
+                        }
                     },
                     y:
                     {
@@ -134,7 +148,18 @@ export const GraphProvider = (props) => {
                             color: 'rgba(0, 0, 0, 0.05)',
                             lineWidth: 1,
                         },
-                        beginAtZero: true
+                        beginAtZero: true,
+                        suggestedMax: function (scale) {
+                            console.log("recompute")
+                            // Get the maximum value in the data set
+                            const max = Math.max(...scale.chart.data.datasets.map(dataset => {
+                                const maxInt = Math.max(...dataset.data.map(el => el.y))
+                                return maxInt
+                            }))
+
+                            // Calculate the suggested maximum by adding 10% to the max value
+                            return max * 1.1;
+                        }
                     },
                 },
                 hover: {
