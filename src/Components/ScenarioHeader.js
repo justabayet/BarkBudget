8d31aef0be71422c3004773e97908107262b45c5
justabayet <anthony.bayet@hotmail.com>
2023-04-05 20:31:15 +0200

    Add scenario name update

--- src/Components/ScenarioHeader.js
+++ src/Components/ScenarioHeader.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Box, IconButton, TextField, Typography } from "@mui/material"
+import { Box, IconButton, Stack, TextField, Typography } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
 import { getFormattedDate } from "../helpers"
@@ -16,6 +16,7 @@ const ScenarioHeader = () => {
     const [startDate, setStartDate] = useState(getFormattedDate(scenario.startDate))
     const [endDate, setEndDate] = useState(getFormattedDate(scenario.endDate))
     const [startAmount, setStartAmount] = useState(scenario.startAmount)
+    const [name, setName] = useState(scenario.name)
 
     const index = scenarios.findIndex(scenarioObj => scenarioObj.id === scenario.id)
 
@@ -27,6 +28,7 @@ const ScenarioHeader = () => {
         setStartDate(getFormattedDate(scenario.startDate))
         setEndDate(getFormattedDate(scenario.endDate))
         setStartAmount(scenario.startAmount)
+        setName(scenario.name)
     }, [scenario])
 
     const save = () => {
@@ -46,6 +48,7 @@ const ScenarioHeader = () => {
         updatedValue.startDate = newStartDate
         updatedValue.endDate = newEndDate
         updatedValue.startAmount = startAmount
+        updatedValue.name = name
         updateScenario(updatedValue, index)
     }
 
@@ -60,78 +63,102 @@ const ScenarioHeader = () => {
             </Box>
 
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: "space-between" }}>
-                <DatePicker
-                    sx={textFieldStyle}
-                    onAccept={(newStartDateValue) => {
-                        setStartDate(newStartDateValue.format('YYYY-MM-DD'))
+                <Stack spacing={2}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: "space-between" }}>
 
-                        const newStartDate = new Date(newStartDateValue)
-                        if (isNaN(newStartDate)) {
-                            console.log("Invalid date", newStartDateValue)
-                            return
-                        }
-                        const { ...updatedValue } = scenario
-                        updatedValue.startDate = newStartDate
-                        updatedValue.endDate = new Date(endDate)
-                        updatedValue.startAmount = startAmount
-                        updateScenario(updatedValue, index)
-                    }}
-                    value={dayjs(startDate)}
-                    slotProps={{
-                        textField: {
-                            size: "small",
-                            onChange: (newStartDateValue) => {
+                        <DatePicker
+                            sx={textFieldStyle}
+                            label="Starting Date"
+                            onAccept={(newStartDateValue) => {
                                 setStartDate(newStartDateValue.format('YYYY-MM-DD'))
-                            },
-                            onBlur: save
-                        }
-                    }}
-                    format="DD-MM-YYYY" />
 
-                <DatePicker
-                    sx={textFieldStyle}
-                    onAccept={(newEndDateValue) => {
-                        setEndDate(newEndDateValue.format('YYYY-MM-DD'))
+                                const newStartDate = new Date(newStartDateValue)
+                                if (isNaN(newStartDate)) {
+                                    console.log("Invalid date", newStartDateValue)
+                                    return
+                                }
+                                const { ...updatedValue } = scenario
+                                updatedValue.startDate = newStartDate
+                                updatedValue.endDate = new Date(endDate)
+                                updatedValue.startAmount = startAmount
+                                updateScenario(updatedValue, index)
+                            }}
+                            value={dayjs(startDate)}
+                            slotProps={{
+                                textField: {
+                                    size: "small",
+                                    onChange: (newStartDateValue) => {
+                                        setStartDate(newStartDateValue.format('YYYY-MM-DD'))
+                                    },
+                                    onBlur: save
+                                }
+                            }}
+                            format="DD-MM-YYYY" />
 
-                        const newEndDate = new Date(newEndDateValue)
-                        if (isNaN(newEndDate)) {
-                            console.log("Invalid date", newEndDateValue)
-                            return
-                        }
-                        const { ...updatedValue } = scenario
-                        updatedValue.startDate = new Date(startDate)
-                        updatedValue.endDate = newEndDate
-                        updatedValue.startAmount = startAmount
-                        updateScenario(updatedValue, index)
-                    }}
-                    value={dayjs(endDate)}
-                    slotProps={{
-                        textField: {
-                            size: "small",
-                            onChange: (newEndDateValue) => {
+                        <DatePicker
+                            sx={textFieldStyle}
+                            label="Ending Date"
+                            onAccept={(newEndDateValue) => {
                                 setEndDate(newEndDateValue.format('YYYY-MM-DD'))
-                            },
-                            onBlur: save
-                        }
-                    }}
-                    format="DD-MM-YYYY" />
-                <TextField
-                    variant="outlined"
-                    sx={textFieldStyle}
-                    size="small"
-                    value={startAmount}
-                    onChange={(event) => {
-                        const regex = /^(?!^0\d)-?\d*\.?\d*$|^$/;
-                        if (event.target.value === "" || regex.test(event.target.value)) {
-                            setStartAmount(event.target.value)
-                        }
-                    }}
-                    onBlur={save}
-                />
-                <IconButton onClick={() => deleteScenario(scenario, index)} style={{ "marginLeft": "auto" }}>
-                    <DeleteIcon />
-                </IconButton>
-            </Box>
+
+                                const newEndDate = new Date(newEndDateValue)
+                                if (isNaN(newEndDate)) {
+                                    console.log("Invalid date", newEndDateValue)
+                                    return
+                                }
+                                const { ...updatedValue } = scenario
+                                updatedValue.startDate = new Date(startDate)
+                                updatedValue.endDate = newEndDate
+                                updatedValue.startAmount = startAmount
+                                updateScenario(updatedValue, index)
+                            }}
+                            value={dayjs(endDate)}
+                            slotProps={{
+                                textField: {
+                                    size: "small",
+                                    onChange: (newEndDateValue) => {
+                                        setEndDate(newEndDateValue.format('YYYY-MM-DD'))
+                                    },
+                                    onBlur: save
+                                }
+                            }}
+                            format="DD-MM-YYYY" />
+
+                        <IconButton onClick={() => deleteScenario(scenario, index)} style={{ "marginLeft": "auto" }}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <TextField
+                            variant="outlined"
+                            sx={textFieldStyle}
+                            label="Starting Amount"
+                            size="small"
+                            value={startAmount}
+                            onChange={(event) => {
+                                const regex = /^(?!^0\d)-?\d*\.?\d*$|^$/;
+                                if (event.target.value === "" || regex.test(event.target.value)) {
+                                    setStartAmount(event.target.value)
+                                }
+                            }}
+                            onBlur={save}
+                        />
+
+                        <TextField
+                            variant="outlined"
+                            sx={textFieldStyle}
+                            label="Name"
+                            size="small"
+                            value={name}
+                            onChange={(event) => {
+                                setName(event.target.value)
+                            }}
+                            onBlur={save}
+                        />
+                    </Box>
+                </Stack >
+            </Box >
         </>
     )
 }
