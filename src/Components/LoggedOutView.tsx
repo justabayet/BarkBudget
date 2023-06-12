48260179c8bc8e55b817e1d3f81212b36880cd6a
justabayet <anthony.bayet@hotmail.com>
2023-06-12 20:58:52 +0200

    - color filter on logo

--- src/Components/LoggedOutView.tsx
+++ src/Components/LoggedOutView.tsx
@@ -1,10 +1,12 @@
-import { Box, IconButton } from '@mui/material'
+import { Box, IconButton, useTheme } from '@mui/material'
 import React from 'react'
 import { useAuthentication } from '../Providers/AuthenticationProvider'
 import GoogleIcon from './GoogleLogo'
 
 const LoggedOutView = (): JSX.Element => {
     const { handleSignIn } = useAuthentication()
+    const theme = useTheme()
+    const fileSuffix = theme.palette.mode === "dark" ? 'primary' : 'secondary'
 
     return (
         <Box sx={{
@@ -17,7 +19,7 @@ const LoggedOutView = (): JSX.Element => {
             flexDirection: 'column',
             flexWrap: 'wrap'
         }}>
-            <img className='loginLogo' src="./images/combination-mark-primary.svg" alt="BarkBudget logo" />
+            <img className='loginLogo' src={`./images/combination-mark-${fileSuffix}.svg`} alt="BarkBudget logo" />
             <IconButton sx={{ margin: 'auto' }} onClick={handleSignIn}>
                 <GoogleIcon sx={{ fontSize: '80px' }} />
             </IconButton>
